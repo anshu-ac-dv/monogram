34ec0bf97c921f7080e62c35d92905d395f21bc0
anshu <anshu.ac.dv@gmail.com>
2026-02-18 08:15:16 +0530

    Initial Commit

---
+++ lib/Screens/welcomeScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Welcomescreen extends StatefulWidget {
+  const Welcomescreen({super.key});
+
+  @override
+  State<Welcomescreen> createState() => _WelcomescreenState();
+}
+
+class _WelcomescreenState extends State<Welcomescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text("Suggested post for you", style: GoogleFonts.oswald(fontSize: 15)),
+          )
+        ],
+      ),
+    );
+  }
+}
